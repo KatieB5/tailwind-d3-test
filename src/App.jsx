3f67541483763bc1d8d85b18f90d4d82a6fb0abf
KatieB5 <katie.bickford2@gmail.com>
2024-04-23 08:52:01 +0100

    started work on worldmap component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import {HomePage} from './components/HomePage';
-
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { HomePage } from "./components/HomePage";
+import { WorldMap } from "./components/WorldMap";
 
 function App() {
-
-
   return (
     <>
-      <Header/>
-        <Routes>
-          <Route path="/" element={<HomePage />}/>
-        </Routes>
-      <Footer/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/worldmap" element={<WorldMap />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
